b92067227d4d16e064c3ad66bf576f4d73bfd341
satinder <satinder@Users-MacBook.lan>
2025-12-29 07:46:17 +0000

    feat: Add BaseRtpEndpoint config to enable REMB feedback to camera
    - Created BaseRtpEndpoint.conf.ini with explicit REMB settings
      - rembLocalActivation=true to send REMB to camera
      - rembOnConnect=5000000 (5 Mbps initial estimate)
      - rembMinBitrate=500000 (500 Kbps minimum)
    - Updated start_kurento.sh to mount BaseRtpEndpoint config
      - Added for both macOS and Linux docker run commands
      - Clear REMB_FIX markers for easy rollback
    - Addresses TODO.md issue #2: RTCP Not Flowing to Camera
    - Previous attempts tried SDP changes, this fixes at Kurento config level
    
    ROLLBACK: Remove BaseRtpEndpoint mount line from start_kurento.sh
    or checkout previous branch: fix-rtcp-direction-sendrecv

--- livestreaming/scripts/start_kurento.sh
+++ livestreaming/scripts/start_kurento.sh
@@ -108,10 +108,13 @@ else
             -p ${WS_PORT}:${WS_PORT}/udp \
             -p 5000-5050:5000-5050/udp \
             -v "${CONFIG_DIR}/WebRtcEndpoint.conf.ini:/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini:Z" \
+            -v "${CONFIG_DIR}/BaseRtpEndpoint.conf.ini:/etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini:Z" \
             -e KMS_MIN_PORT=5000 \
             -e KMS_MAX_PORT=5050 \
             -e GST_DEBUG=3,Kurento*:4 \
             ${IMAGE}
+        # REMB_FIX: Added BaseRtpEndpoint.conf.ini mount above to enable REMB feedback
+        # ROLLBACK: Remove the BaseRtpEndpoint mount line to revert
     else
         # Linux: use host network for better performance
         echo "Detected Linux: using host network"
@@ -120,10 +123,13 @@ else
             --name ${CONTAINER_NAME} \
             --network host \
             -v "${CONFIG_DIR}/WebRtcEndpoint.conf.ini:/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini:Z" \
+            -v "${CONFIG_DIR}/BaseRtpEndpoint.conf.ini:/etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini:Z" \
             -e KMS_MIN_PORT=5000 \
             -e KMS_MAX_PORT=5050 \
             -e GST_DEBUG=3,Kurento*:4 \
             ${IMAGE}
+        # REMB_FIX: Added BaseRtpEndpoint.conf.ini mount above to enable REMB feedback
+        # ROLLBACK: Remove the BaseRtpEndpoint mount line to revert
     fi
 
     echo "✅ Container created and started"
